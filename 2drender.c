df9531aaa78f3b87fc491cbfc022d2201bc03cde
Skyler Marks <skyler@bu.edu>
2025-03-03 20:53:56 -0500

    Skeleton

---
+++ 2drender.c
@@ -0,0 +1,23 @@
+#include "raylib.h"
+#include <stdio.h>
+#include "raymath.h"
+
+# define WIDTH 1000
+# define HEIGHT 1000
+# define DEPTH 1000
+
+
+int eqinbox (const char* e, Rectangle r){
+	return 0;
+}
+
+int main () {
+		InitWindow(WIDTH, HEIGHT, "Cloth - Sim");
+		SetExitKey(KEY_Q);
+	while (!WindowShouldClose()) {
+	BeginDrawing();
+		ClearBackground(BLACK);
+	CloseWindow();
+	return 0;
+}}
+
